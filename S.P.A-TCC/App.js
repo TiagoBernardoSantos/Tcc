c420ffd3dcffa6638ab438523248771e3d3c0ed0
Tigas2 <tiagobsantos154@gmail.com>
2023-06-22 14:00:08 -0300

    add projeto

---
+++ S.P.A-TCC/App.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Text, View, StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+
+import Contacts from './src/pages/Contacts/'
+import Information from './src/pages/Information/'
+
+/* import Cadastro from './components/cadastro.js';
+ */
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Cadastro />   
+    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator> 
+        <Stack.Screen name="Contacts" component={Contacts}/>
+
+        <Stack.Screen name="Information" component={Information}/>
+      </Stack.Navigator> 
+
+      </Stack>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+ 
+});
---
+++ S.P.A-TCC/components/login.js
@@ -0,0 +1,92 @@
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txtTitle}> Conecte-se </Text>
+      <StatusBar style="auto" />
+      <Text style={{fontSize: 17}}>E-mail:</Text>
+      <TextInput style={styles.formInput}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoComplete="email"
+        placeholder='Digite seu e-mail...'
+      />
+      <Text style={{fontSize: 17}}>Senha:</Text>
+      <TextInput style={styles.formInput}
+        secureTextEntry
+        autoCapitalize="none"
+        placeholder='Digite sua senha...'
+        autoComplete="password"
+      />
+      <Pressable style={styles.formButton}>
+        <Text style={styles.txtButton}> Entrar </Text>
+      </Pressable>
+      <View style={styles.subContainer}>
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTxtButton}> Esqueceu sua senha? </Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.subContainer}>
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTxtButton}> Inscrever-se </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txtTitle: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    margin: 40,
+  },
+  formInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 22,
+    width: '80%',
+    height:'7%',
+    padding: 10,
+    margin: 10,
+  },
+  formButton: {
+    backgroundColor: '#114D9D',
+    width: '80%',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  txtButton: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  subContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  subButton: {
+    padding: 10,
+  },
+  subTxtButton: {
+    color: '#FEB74E',
+    marginLeft: 50,
+    fontSize: 17,
+    alignItems: 'center',
+  },
+ 
+});
