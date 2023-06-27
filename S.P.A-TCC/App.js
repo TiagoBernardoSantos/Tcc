6d52b7e2e65b6353d78248b6da4913e98620e491
beatriz <beatriz.oliveira.lhbo@gmail.com>
2023-06-27 17:11:51 -0300

    desc

--- S.P.A-TCC/App.js
+++ S.P.A-TCC/App.js
@@ -1,26 +1,47 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { Text, View, StyleSheet } from 'react-native'
-import Constants from 'expo-constants'
+import { Text, View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
-
-import Cadastro from './components/cadastro';
-import Login from './components/login';
-import Inicial from './components/inicial';
-
+import Cadastro from "./components/cadastro";
+import Login from "./components/login";
+import Inicial from "./components/inicial";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator > 
-        <Stack.Screen name="Login" component={Login} options={{title: '', headerTransparent: true, headerLeft: null}}/>
-        <Stack.Screen name="Cadastro" component={Cadastro} options={{title: '', headerTransparent: true, headerLeft: null}} />
-        <Stack.Screen name="Inicial" component={Inicial} options={{title: 'Home', headerTransparent: false, headerLeft: null}} />
-      </Stack.Navigator> 
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "", headerTransparent: true, headerLeft: null }}
+        />
+
+        <Stack.Screen
+          name="Cadastro"
+          component={Cadastro}
+          options={{ title: "", headerTransparent: true, headerLeft: null }}
+        />
+
+        <Stack.Screen
+          name="Inicial"
+          component={Inicial}
+          options={{
+            title: "Home",
+            headerTransparent: false,
+            headerLeft: null,
+            headerTintColor: "#fff",
+            headerFontSize: "300px",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#114D9D", height: 90 },
+            headerTitleStyle: { backgroundColor: "#114D9D", fontSize: 30 },
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -28,10 +49,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 8,
   },
- 
 });
