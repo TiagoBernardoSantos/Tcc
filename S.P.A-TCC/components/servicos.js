b577d54231b34450b4e14b08555a5d8ba228ef11
Maria Luiza <Tigas2>
2023-07-20 17:56:15 -0300

    desc

--- S.P.A-TCC/components/servicos.js
+++ S.P.A-TCC/components/servicos.js
@@ -1,35 +1,122 @@
-import { StatusBar } from "expo-status-bar";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  ImageBackground,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
-
-const image = { require: ('./assets/map.png') };
-/* https://reactnative.dev/docs/imagebackground */
+import { Image } from 'expo-image';
+import { StatusBar } from 'expo-status-bar';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function Serviços({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={image}
-        resizeMode="cover"
-        style={styles.image}
-      ></ImageBackground>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.subContainer}>
+
+             <Image
+                style={styles.image}
+                source={require('../assets/policia.png')}
+            />
+
+            <TouchableOpacity style={styles.formButton}>
+                <Text style={styles.txtButton} onPress={() => navigation.navigate('Instruções')}> Ligar </Text>
+                <MaterialCommunityIcons
+                name="telephone"
+                size={20} />
+            </TouchableOpacity> 
+            </View>
+            
+            <View style={styles.subContainer}>
+             <Image
+                style={styles.image}
+                source={require('../assets/ambulancia.png')}
+            />
+
+            <TouchableOpacity style={styles.formButton}>
+                <Text style={styles.txtButton} onPress={() => navigation.navigate('Instruções')}> Ligar </Text>
+            </TouchableOpacity> 
+            </View>
+
+            <View style={styles.subContainer}>
+             <Image
+                style={styles.image}
+                source={require('../assets/cvv.png')}
+            />
+
+            <TouchableOpacity style={styles.formButton}>
+                <Text style={styles.txtButton} onPress={() => navigation.navigate('Instruções')}> Ligar </Text>
+            </TouchableOpacity> 
+            </View>
+
+            <View style={styles.subContainer}>
+             <Image
+                style={styles.image}
+                source={require('../assets/bombeiro.png')}
+            />
+
+            <TouchableOpacity style={styles.formButton}>
+                <Text style={styles.txtButton} onPress={() => navigation.navigate('Instruções')}> Ligar </Text>
+            </TouchableOpacity> 
+            </View>
+        </View>
+    );
 }
-/* roda pé: https://www.youtube.com/watch?v=AnjyzruZ36E */
+
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    image: {
+        margin: 15,
+        flex: 1,
+        width: '70%',
+    },
+
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    txtTitle: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        marginTop: 60,
+        color: '#FEB74E',
+
+    },
+
+    formButton: {
+        backgroundColor: '#FEB74E',
+        width: '50%',
+        margin: 30,
+        padding: 10,
+        borderRadius: 70,
+        alignItems: 'center',
+    },
+
+    txtButton: {
+        color: '#fff',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+
+    subContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '80%',
+    },
+
+    subButton: {
+        padding: 10,
+    },
+
+    subTxtButton: {
+        color: '#FEB74E',
+        marginLeft: 50,
+        fontSize: 17,
+        alignItems: 'center',
+    },
+
+    txt: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        margin: 25,
+    },
 });
