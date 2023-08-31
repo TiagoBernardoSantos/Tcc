6feb86fa49ea43d0dd5ea9292af8c3e8aec9c338
Manoelzinho <canetaazul.aps@gmail.com>
2023-08-31 14:38:46 -0300

    MAPA

--- S.P.A-TCC/components/inicial.js
+++ S.P.A-TCC/components/inicial.js
@@ -1,32 +1,45 @@
+import { requestMultiple, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from 'react-native-geolocation-service';
 import { StatusBar } from "expo-status-bar";
+import React from 'react';
+import MapView from "react-native-maps";
+import { Dimensions } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import {
   Pressable,
   ScrollView,
   StyleSheet,
-  ImageBackground,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  Platform,
+  useState, 
+  useEffect,
 } from "react-native";
 
-const image = { require: "../assets/map.png" };
-/* https://reactnative.dev/docs/imagebackground */
 
 export default function Inicial({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={styles.subConteiner}>
-        <Text style={styles.text}>Home</Text>
-      </View>
 
-      <ImageBackground
-        source={require("../assets/map.png")}
-        style={styles.image}
-        resizeMode="cover"
-      >
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+
+      <View style={styles.subConteiner}>
+       
+          <Text style={styles.text}>Home</Text>
+        
+
+
         <View style={styles.subContainerbutton}>
           <View style={styles.button}>
             <TouchableOpacity>
@@ -36,17 +49,14 @@ export default function Inicial({ navigation }) {
                 color={"white"}
                 alignItems="center"
                 justifyContent="center"
-              
+
                 onPress={() => navigation.navigate('Chat')}>
 
-                </MaterialCommunityIcons>
-              
-              
+              </MaterialCommunityIcons>
             </TouchableOpacity>
           </View>
         </View>
-      </ImageBackground>
-      
+      </View>
     </View>
   );
 }
@@ -60,22 +70,20 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "15%",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
-  image: {
-    width: "100%",
-    height: "100%",
-  },
+  
   text: {
     color: "#fff",
     fontSize: 40,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   subContainerbutton: {
     alignItems: "flex-end",
     justifyContent: "flex-start",
     margin: 390,
-    marginTop: 460,
+    marginTop: 1360,
+    marginRight: 50,
   },
   button: {
     backgroundColor: "#114D9D",
@@ -84,5 +92,10 @@ const styles = StyleSheet.create({
     borderRadius: 150,
     alignItems: "center",
     justifyContent: "center",
+
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    height: Dimensions.get("window").height,
   },
 });
